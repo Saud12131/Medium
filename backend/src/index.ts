36cbcaab390fc105478164f8bc02428908590dc4
saud sayyed <155637796+Saud12131@users.noreply.github.com>
2024-12-22 00:15:54 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,86 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+    JWT_SECRET: String
+  }
+}>()
+
+app.get('/', async (c) => {
+
+  return c.text('Hello Hono!')
+})
+
+app.use("/api/v1/blog/*", async (c, next) => {
+  let header = c.req.header("Authorization") || "";
+  const token = header.split(" ")[1];
+  //@ts-ignore
+  let response = await verify(token, c.env.JWT_SECRET);
+  if (response.id) {
+    next();
+  } else {
+    c.status(403)
+    return c.json({
+      error: "unauthorized"
+    });
+  }
+})
+
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      password: body.password
+    },
+  });
+  //@ts-ignore
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({
+    jwt: token
+  })
+})
+
+app.post('/api/v1/login', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    }
+  });
+  if (!user) {
+    c.status(403);
+    return c.json({
+      error: "user not found"
+    });
+  }
+  //@ts-ignore
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({
+    jwt
+  })
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default app
